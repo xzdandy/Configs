5494afc07d7e96fa03dfa7ba134938ab40444af0
xzdandy <xzdandy@gmail.com>
2019-06-04 17:45:55 -0400

    update man page color

--- configfiles/dotfiles/config.fish
+++ configfiles/dotfiles/config.fish
@@ -8,7 +8,7 @@ fish_vi_key_bindings
 
 # Alias #
 alias rm="safe-rm"
-alias g='googler -n 5 --colors nXigxy'
+alias g='googler -n 5 --colors nXigxb'
 
 # PATH #
 set PATH $PATH $HOME/.cargo/bin
@@ -22,6 +22,15 @@ source /home/xzdandy/.config/fish/functions/theme-pure/conf.d/pure.fish
 set fish_pager_color_prefix red --bold --underline
 set fish_color_selection white --bold --background=grey
 set fish_color_search_match bryellow --background=grey
+
+# Colorful man page
+set -x LESS_TERMCAP_mb (set_color brred)
+set -x LESS_TERMCAP_md (set_color brred)
+set -x LESS_TERMCAP_me (set_color normal)
+set -x LESS_TERMCAP_se (set_color normal)
+set -x LESS_TERMCAP_so (set_color -b normal brcyan)
+set -x LESS_TERMCAP_ue (set_color normal)
+set -x LESS_TERMCAP_us (set_color brgreen)
 
 # Start X at login #
 if status --is-login
